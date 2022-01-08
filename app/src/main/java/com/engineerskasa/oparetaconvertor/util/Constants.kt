ec2921c6e887fa1b79e0c27b8d998ea17118a18b
dedongh <slimshades68@gmail.com>
2022-01-08 02:04:53 +0000

    --Added unit tests

--- app/src/main/java/com/engineerskasa/oparetaconvertor/util/Constants.kt
+++ app/src/main/java/com/engineerskasa/oparetaconvertor/util/Constants.kt
@@ -1,6 +1,6 @@
 package com.engineerskasa.oparetaconvertor.util
 
-const val API_KEY = "2e913144-74c4-418d-b347-5d48f7af4384"
+const val API_KEY = ""
 const val DATABASE_NAME = "opareta_db"
 const val BASE_URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/"
 
